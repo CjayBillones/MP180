3b68f383602c821d1eb2ffe35eb77b826e2ffe8f
Genesis Palaganas <itetsky123@gmail.com>
2014-12-08 01:47:33 +0800

    Added StudentDataInitializer

---
+++ ANNdroid/src/StudentDataInitializer.java
@@ -0,0 +1,34 @@
+package ANNdroid.src;
+
+import java.util.*;
+
+class StudentDataInitializer{
+	
+	public static void main(String args[]){
+
+		Simulator sim = new Simulator();
+		StudentData sd = new StudentData();
+
+		List<Student> studentList = new LinkedList<Student>();
+
+		for(User user: sim.userList){
+
+			if(user instanceof Student)
+				studentList.add((Student)user);
+
+		}
+
+
+		for(Student student: studentList){
+
+			Map<Categories, Object> studentInfo = new HashMap<Categories, Object>();
+			sd.put(student,studentInfo);
+			
+		}
+
+		SDataSL.save(sd);
+
+
+	}
+
+}
