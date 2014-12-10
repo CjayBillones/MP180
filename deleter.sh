35c7149ea8ae6592b8fe3f605bd2e52fc7354f1c
CjayBillones <billonesciprian08@gmail.com>
2014-12-10 12:45:56 +0800

    script file for deleting class files

---
+++ deleter.sh
@@ -0,0 +1,9 @@
+rm ./ANNdroid/src/*.class
+rm ./ANNdroid/src/panels/*.class
+rm ./ANNdroid/src/util/*.class
+rm ./ANNdroid/src/objects/*.class
+rm ./ANNdroid/src/events/*.class
+rm ./ANNdroid/src/custom_swing/*.class
+rm ./ANNdroid/src/ai/aimaker/*.class
+rm ./ANNdroid/src/ai/ann/*.class
+rm ./ANNdroid/src/ai/search/*.class
